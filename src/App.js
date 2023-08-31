99443cf054051f797aff82840d37143c397c0779
mkalwar <khandelwal.monika@gmail.com>
2023-09-01 00:47:06 +0530

    LawFirm code added

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import NavBar from "./components/Navbar/NavBar";
+import HeroSection from "./components/HeroSection/HeroSection";
+import Intro from "./components/Intro/Intro";
+import SuccessStoryCard from "./components/SuccessStory/SuccessStoryCard";
+import AreaOfPractise from "./components/AreaOfPractise/AreaOfPractise";
+import ClientCard from "./components/ClientCard/ClientCard";
+import TeamCards from "./components/OurTeam/TeamCards";
+import Faq from "./components/Faq/Faq";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+        <hr />
+        <HeroSection />
+        <Intro />
+        <SuccessStoryCard />
+        <AreaOfPractise />
+        <ClientCard />
+        <TeamCards />
+        <Faq />
+        <Subscribe />
+        <Footer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
